12b228a18214854fe50a5968473f41fef0cda290
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-03 13:18:52 -0500

    Create SuppressPropertyNotSetInConstructorHook.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Hook/SuppressPropertyNotSetInConstructorHook.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\PsalmPlugin\Hook;
+
+use Ghostwriter\PsalmPlugin\AbstractHook;
+
+use PHPUnit\Framework\TestCase;
+use Psalm\Issue\PropertyNotSetInConstructor;
+use Psalm\Plugin\EventHandler\BeforeAddIssueInterface;
+use Psalm\Plugin\EventHandler\Event\BeforeAddIssueEvent;
+
+final class SuppressPropertyNotSetInConstructorHook extends AbstractHook implements BeforeAddIssueInterface
+{
+    public static function beforeAddIssue(BeforeAddIssueEvent $event): ?bool
+    {
+        $codeIssue = $event->getIssue();
+        if (! $codeIssue instanceof PropertyNotSetInConstructor) {
+            return self::CONTINUE;
+        }
+
+        $propertyId = $codeIssue->property_id;
+
+        [$className, $propertyName] = explode('::$', $propertyId);
+
+        $codebase = $event->getCodebase();
+
+        if (! $codebase->classExtends($className, TestCase::class)) {
+            return self::CONTINUE;
+        }
+
+        $classLikeStorage = $codebase->classlikes->getStorageFor($className);
+        if (array_key_exists($propertyName, $classLikeStorage->methods['setup']->this_property_mutations ?? [])) {
+            return self::SUPPRESS;
+        }
+
+        return self::CONTINUE;
+    }
+}
