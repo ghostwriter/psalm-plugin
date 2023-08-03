cc7119cd5211e4668cd899300189408b97baa577
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-03 13:19:18 -0500

    Create FixParamNameMismatchHook.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Hook/FixParamNameMismatchHook.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\PsalmPlugin\Hook;
+
+use Ghostwriter\PsalmPlugin\AbstractHook;
+
+
+use Psalm\Plugin\EventHandler\BeforeAddIssueInterface;
+use Psalm\Plugin\EventHandler\Event\BeforeAddIssueEvent;
+
+final class FixParamNameMismatchHook extends AbstractHook implements BeforeAddIssueInterface
+{
+    public static function beforeAddIssue(BeforeAddIssueEvent $event): ?bool
+    {
+        return self::CONTINUE;
+    }
+}
