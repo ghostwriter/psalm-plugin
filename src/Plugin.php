9bae041d52d0c826ad66bb3022b55277345e8657
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-04 11:57:38 -0500

    Update Plugin.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,31 +4,17 @@ declare(strict_types=1);
 
 namespace Ghostwriter\PsalmPlugin;
 
-use FilesystemIterator;
 use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
 use SimpleXMLElement;
-use SplFileInfo;
 
 final class Plugin implements PluginEntryPointInterface
 {
     public function __invoke(RegistrationInterface $registration, ?SimpleXMLElement $config = null): void
     {
-        foreach (new FilesystemIterator(__DIR__ . DIRECTORY_SEPARATOR . 'Hook', FilesystemIterator::SKIP_DOTS) as $file) {
-            if (! $file instanceof SplFileInfo) {
-                continue;
-            }
-
-            if (! $file->isFile()) {
-                continue;
-            }
-
-            if (! str_ends_with($file->getPathname(), '.php')) {
-                continue;
-            }
-
+        foreach (glob(__DIR__ . '/Hook/*.php') as $file) {
             /** @var class-string $class */
-            $class = sprintf('%s\\Hook\\%s', __NAMESPACE__, $file->getBasename('.php'));
+            $class = __NAMESPACE__ . '\\Hook\\' . basename($file, '.php');
 
             if (! class_exists($class)) {
                 return;
