56131caa3b9062782f48a896de9920da47b5bdc6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-10 15:33:36 -0500

    Update AbstractHook.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/AbstractHook.php
+++ src/AbstractHook.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Ghostwriter\PsalmPlugin;
 
+use PhpParser\Comment\Doc;
+use PhpParser\Node;
+use Psalm\Codebase;
+use Psalm\DocComment;
+use Psalm\Internal\Scanner\ParsedDocblock;
+
 abstract class AbstractHook
 {
     final public const IGNORE = null;
@@ -11,4 +17,40 @@ abstract class AbstractHook
     final public const REPORT = true;
 
     final public const SUPPRESS = false;
+
+    public static function fullyQualifiedClassName(string $className, Codebase $codebase): string
+    {
+        return $codebase->classlikes->getUnAliasedName($className);
+    }
+
+    public static function getStatements(string $filePath, Codebase $codebase): array
+    {
+        return $codebase->statements_provider
+            ->getStatementsForFile(
+                $filePath,
+                $codebase->analysis_php_version_id
+            );
+    }
+
+    public static function hasFullyQualifiedClassName(string $className, Codebase $codebase): bool
+    {
+        return $codebase->classlikes->hasFullyQualifiedClassName($className);
+    }
+
+    public static function isClassReferenced(string $className, Codebase $codebase): bool
+    {
+        $fullyQualifiedClassName = self::fullyQualifiedClassName($className, $codebase);
+
+        return $codebase->file_reference_provider->isClassReferenced(mb_strtolower($fullyQualifiedClassName));
+    }
+
+    public static function parseDocComment(Node $node): ?ParsedDocblock
+    {
+        $doc = $node->getDocComment();
+        if (! $doc instanceof Doc) {
+            return null;
+        }
+
+        return DocComment::parsePreservingLength($doc);
+    }
 }
