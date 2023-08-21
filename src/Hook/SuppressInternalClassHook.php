4e180a6b1da02a7cfa9343efa2899349bc1a9478
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-21 08:52:16 -0500

    Update SuppressInternalClassHook.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Hook/SuppressInternalClassHook.php
+++ src/Hook/SuppressInternalClassHook.php
@@ -5,20 +5,19 @@ declare(strict_types=1);
 namespace Ghostwriter\PsalmPlugin\Hook;
 
 use Ghostwriter\PsalmPlugin\AbstractBeforeAddIssueEventHook;
-
 use Psalm\Issue\InternalClass;
 use Psalm\Plugin\EventHandler\Event\BeforeAddIssueEvent;
 
 final class SuppressInternalClassHook extends AbstractBeforeAddIssueEventHook
 {
     /**
-     * @return null|false
+     * @return false|null
      */
     public static function beforeAddIssue(BeforeAddIssueEvent $event): ?bool
     {
         $codeIssue = $event->getIssue();
 
-        if (! $codeIssue instanceof InternalClass) {
+        if (!$codeIssue instanceof InternalClass) {
             return self::IGNORE;
         }
 
