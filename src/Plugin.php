cc12b1665023a4a6b51da88a9e388022c5dad8ba
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-21 08:51:55 -0500

    Update Plugin.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Plugin.php
+++ src/Plugin.php
@@ -6,17 +6,16 @@ namespace Ghostwriter\PsalmPlugin;
 
 use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
-use SimpleXMLElement;
 
 final class Plugin implements PluginEntryPointInterface
 {
-    public function __invoke(RegistrationInterface $registration, ?SimpleXMLElement $config = null): void
+    public function __invoke(RegistrationInterface $registration, \SimpleXMLElement $config = null): void
     {
-        foreach (glob(__DIR__ . '/Hook/*.php') ?: [] as $file) {
+        foreach (glob(__DIR__.'/Hook/*.php') ?: [] as $file) {
             /** @var class-string $class */
-            $class = __NAMESPACE__ . '\\Hook\\' . basename($file, '.php');
+            $class = __NAMESPACE__.'\\Hook\\'.basename($file, '.php');
 
-            if (! class_exists($class)) {
+            if (!class_exists($class)) {
                 return;
             }
 
