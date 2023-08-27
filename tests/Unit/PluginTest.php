423b2faa350b9d9f02390cc017c8f7cebbbbede1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-26 19:42:33 -0500

    Update PluginTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/PluginTest.php
+++ tests/Unit/PluginTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\PsalmPlugin\Tests\Unit;
 
+use CallbackFilterIterator;
+use FilesystemIterator;
+use Generator;
 use Ghostwriter\PsalmPlugin\AbstractBeforeAddIssueEventHook;
 use Ghostwriter\PsalmPlugin\AbstractHook;
 use Ghostwriter\PsalmPlugin\Hook\FixParamNameMismatchHook;
@@ -18,9 +21,13 @@ use Ghostwriter\PsalmPlugin\Hook\SuppressUnusedClassHook;
 use Ghostwriter\PsalmPlugin\Plugin;
 use Ghostwriter\PsalmPluginTester\Fixture;
 use Ghostwriter\PsalmPluginTester\PluginTester;
+use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use SplFileInfo;
+use function dirname;
+use function realpath;
 
 #[CoversClass(Plugin::class)]
 #[CoversClass(AbstractBeforeAddIssueEventHook::class)]
@@ -44,13 +51,40 @@ final class PluginTest extends TestCase
     }
 
     /**
-     * @return \Generator<string,list<Fixture>>
+     * @return Generator<string,array<Fixture>>
      */
-    public static function fixtureDataProvider(): \Generator
+    public static function fixtureDataProvider(): Generator
     {
-        yield from PluginTester::yieldFixtures(
-            dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'fixtures'
-        );
+        $levels = 0;
+
+        $path = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'fixtures';
+
+        $vendorDirectory = realpath($path . '/vendor');
+
+        while ($vendorDirectory === false) {
+            $vendorDirectory = realpath(dirname($path, ++$levels) . '/vendor');
+        }
+
+        if ($vendorDirectory === '') {
+            Assert::fail('Could not find vendor directory');
+        }
+
+        foreach (new FilesystemIterator($path, FilesystemIterator::SKIP_DOTS) as $fixtureDirectory) {
+            if (! $fixtureDirectory instanceof SplFileInfo) {
+                continue;
+            }
+
+            if (! $fixtureDirectory->isDir()) {
+                continue;
+            }
+
+            yield $fixtureDirectory->getBasename() => [
+                new Fixture(
+                    $fixtureDirectory->getRealPath(),
+                    $vendorDirectory
+                ),
+            ];
+        }
     }
 
     #[DataProvider('fixtureDataProvider')]
