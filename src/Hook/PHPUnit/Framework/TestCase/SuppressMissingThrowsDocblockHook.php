d8c787fe0f9531bff7cb8bdcf6796eec32d8ceea
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-26 21:20:05 -0500

    Create SuppressMissingThrowsDocblockHook.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Hook/PHPUnit/Framework/TestCase/SuppressMissingThrowsDocblockHook.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\PsalmPlugin\Hook\PHPUnit\Framework\TestCase;
+
+use Ghostwriter\PsalmPlugin\AbstractBeforeAddIssueEventHook;
+use PHPUnit\Framework\TestCase;
+use Psalm\Issue\MissingThrowsDocblock;
+use Psalm\Plugin\EventHandler\Event\BeforeAddIssueEvent;
+
+final class SuppressMissingThrowsDocblockHook extends AbstractBeforeAddIssueEventHook
+{
+    /**
+     * @return false|null
+     */
+    public static function beforeAddIssue(BeforeAddIssueEvent $event): ?bool
+    {
+        $codeIssue = $event->getIssue();
+
+        if (!$codeIssue instanceof MissingThrowsDocblock) {
+            return self::IGNORE;
+        }
+
+        $codeLocation = $codeIssue->code_location;
+
+        $codebase = $event->getCodebase();
+
+        foreach ($codebase->classlike_storage_provider->getAll() as $storage) {
+            if (null === $storage->location) {
+                continue;
+            }
+
+            if ($storage->location->file_path !== $codeLocation->file_path) {
+                continue;
+            }
+
+            if (!$codebase->classExtends($storage->name, TestCase::class)) {
+                continue;
+            }
+
+            return self::SUPPRESS;
+        }
+
+        return self::IGNORE;
+    }
+}
